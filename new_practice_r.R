f8cc86728bf32c71f54219d2f091ad51dedd8f99
commonpsychology <commonpsychology@users.noreply.github.com>
2026-01-08 19:30:07 +0545

    93-113 20 pages comitted

--- new_practice_r.R
+++ new_practice_r.R
@@ -212,3 +212,169 @@ del_sixth_layer
 #r practice everything and no karma or work is small can do anything nothing matters.
 #consistent discipline flow for life
 #go out work flow do whatever you want to do as 432 without thinking randomness you like
+#the more you get close to 432, get more use to it, exposed, see, practice,use,it magic happens
+#what you do alone and when noone is watching behind the scenes??
+foo<- c(3,4,1,5,6,7,3,4,6,4,3,2,3)
+bax<- c(4,6,3,6,4,1,2,2,1,2,2,2.3)
+length(foo)
+foo<3
+foo>3
+foo+10>3
+foo[3]
+foo[c(1)]
+foo[c(10,1)]
+foo[-c(6)]
+length(foo)
+foo>bax
+# any() returns true if any true, all returns true if all values true in a vector
+any(foo<3)
+all(foo<3)
+#excercise 4.1
+four_six<- c(6,9,7,3,6,7,9,6,3,6,6,7,1,9,1)
+four_six < 6
+four_six>=6
+four_six < (6+2)
+four_six != 6
+new_vec_four_six <- four_six[-c(1,2,3)]
+new_vec_four_six_array <- array(data=new_vec_four_six,dim=c(2,2,3))
+new_vec_four_six_array
+new_vec_four_six_array <= (6/2)+4
+new_vec_four_six_array+2 <=(6/2)+4
+ten_by_ten <- diag(10)
+ten_by_ten
+ten_by_ten_locations <- ten_by_ten == 0
+ten_by_ten_locations
+which(ten_by_ten==0,arr.ind=TRUE)
+any(new_vec_four_six_array)
+all(new_vec_four_six_array)
+diag_element <- diag(ten_by_ten_locations)
+diag_element
+any(diag_element)
+#excercise 4.2
+foo<- c(7,1,7,10,5,9,10,3,10,8)
+foo>5|foo==2
+foo[foo>5|foo==2]
+vec_indx_logic <-c(T,T,F,T,F,T,F,T,F)
+vec_indx_logic[c(T,T,T,T,T,F,T,F,T)]#WHICHEVER ARE TRUE IN POSITION EXTRACTS ELEMENTS
+bar_new<- c(8,8,4,4,5,1,5,6,6,8)
+bar_new[bar_new<=6&bar_new!=4] #vec[inx ma vec logic halna milxa to extract]
+cond_foo<-foo>5|foo==2
+cond_bar<-bar_new<=6&bar_new!=4
+cond_bar
+cond_both<-cond_foo & cond_bar
+cond_both
+list(
+  foo[cond_both],
+  bar_new[cond_both]
+)
+#give time in confusion give break never give up give whole day write to understand magic
+#in patience taking break listening song mind off it giving up or leaving or 
+#is not the option ever ever slow steady increments daily for life every field 432
+baz <- foo+bar_new
+baz
+elemnt_baz<- baz >=14 & baz !=15
+baz[elemnt_baz]
+div<-baz/foo
+div
+div_elemetn<-div >4 | div<=2
+div[div_elemetn]
+myvec <- c(5,-2.3,4,4,4,6,8,10,40221,-8)
+# T F T F T F T F T F 
+myvec[c(T,F)]# T AND F REPEATS UNTIL THE LAST ELEMENT IN VECTOR WHILE INDEXING
+myvec[(myvec>0) & (myvec<10)]
+myvec[myvec<0]<--200
+myvec
+which(myvec<10)
+which(x=c(T,F,F,T,T))#returns the index of any vector argument which has logical T present
+index<-which(myvec<0)
+myvec[index]
+A <- matrix(c(0.3,4.5,55.3,91,0.1,105.5,-4.2,8.2,27.9),nrow=3,ncol=3)
+A[c(T,F,F),c(F,T,T)]
+A<1
+A[A<1] <- -7
+A
+A>25
+which(A>25)
+A[,1]
+A[,2]
+A[,3]
+which(A>25,arr.ind=TRUE)
+#excercise
+foo <- c(7,5,6,1,2,10,8,3,8,2)
+indx<-foo>=5
+indx
+bar<-foo[indx]
+bar
+foo[-which(foo>=5)]
+baz<-matrix(bar,nrow=2,ncol=3,byrow=TRUE)
+baz
+baz==8
+positions<-which(baz==8,arr.ind = TRUE)
+positions
+baz[1,2]
+baz[positions]<-baz[1,2]^2
+baz
+confirm<-baz<=25&baz>4
+confirm
+baz[confirm]
+qux<-array(c(10,5,1,4,7,4,3,3,1,3,4,3,1,7,8,3,7,3),dim=c(3,2,3))
+dim(qux)
+which(qux==3,arr.ind=TRUE)
+indexes<-which(qux<3|qux>=7,arr.ind = TRUE)
+indexes
+qux[indexes]<-100
+qux
+foo <- c(7,5,6,1,2,10,8,3,8,2)
+foo[c(F,T)]
+foo[c(0,1)]
+char<-"32.3"
+nchar(char)
+length(char)
+alpha==alpha
+"alpha"=="alpha"
+char_vec<- c("sanjeev","dipak","milan")
+char_vec
+length(char_vec)
+nchar(char_vec)
+char_vec=="dipak"
+"alpha"<="beta"
+"alp"<="beta"#compare length and use the logical argument
+"alp">"beta"
+#upper case greater than lower case, ASCII value is the reason
+baz <- "&4 _ 3 **%.? $ymbolic non$en$e ,; "
+baz
+cat("WE","are",char_vec) 
+paste("we","are",char_vec)
+paste("I","am",char_vec)
+paste("I","am","sanjeev")
+paste(char_vec,"Neupane",sep="")
+paste(char_vec,"Neupane","Pokhrel","Rai",sep="00")
+cat("Do","you","think","R is awesome?!!",sep=" ")
+cat("Do","you","think","R is awesome?!!",7+4,sep=" ")
+#escape sequences \n new line \t tab \b backspace \\ forward slash \" quotation marks
+cat("here is a string\nsplit\tto new\b\n\n\tlines")
+cat("here is a string\nsplit\tto new\n\n\tlines")
+setwd("/D/dfdkd/dkdfk")
+substr(baz,start=4,stop=9)
+new_str="Added string"
+substr(baz,start=1,stop=length(new_str))<-new_str
+baz
+sub(pattern="bolic",replacement ="mmm",baz)
+gsub(pattern="d",replacement="s",baz)
+cat("The quick brown fox\n    jumped over\n\t the lazy dog")
+to_sub<-"/Users/tdavies/Documents/RBook/"
+sub(pattern="tdavies",replacement="sneupane",to_sub)
+bar <- "How much wood could a woodchuck chuck"
+glue_word<-"if a woodchuck could chuck wood"
+paste(bar,glue_word)
+sub(pattern="wood",replacement = "metal",paste(bar,glue_word))
+last_string<-"Two 6-packs for $12.99"
+substring(last_string,first=5,last=10)
+substr(last_string,start=length(last_string),stop=5)
+substitute(last_string,"$12.99","$10")
+sub(pattern="12.99",replacement="10",last_string)
+#nice and easy practice like cricketer max daily hours and hours of steady buildup
+#in stocks psychology gaun gym youga writng reading articualtio
+#took 23 years to be in this worse spot wil lbe faster with self 432
+#muscle memory build at steady flow with daily 20 pages minimum practice
+#pg no 13 bata next
